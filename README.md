7f16394b521aee826f96b7ca01aa3f3853c08cf1
wangheling <1915993172@qq.com>
2020-09-29 15:28:17 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kafka-springboot-learning
+
+## 参考文章
+[Kafka在SpringBoot中的整合配置详解](https://www.jianshu.com/p/92487ba9052f?utm_campaign=haruki)
--- kafka-producer/src/main/java/com/heling/controller/TestController.java
+++ kafka-producer/src/main/java/com/heling/controller/TestController.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-/**
- * @Desc：
- * @Author: heling
- * @Date: 2020/9/18 10:47
- */
 @RestController
 public class TestController {
 
     @Resource
     private KafkaTemplate<String,Object> kafkaTemplate;
 
-    //http://localhost:6666/message/send?message=消息
+    //http://localhost:1111/message/send?message=消息
     @GetMapping("/message/send")
     public boolean send(@RequestParam String message){
         kafkaTemplate.send("testTopic",message);
